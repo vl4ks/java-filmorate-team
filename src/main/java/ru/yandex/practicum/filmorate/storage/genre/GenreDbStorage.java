fa20df43f054429eac950a1f23ea68e0428de3b1
Nikolay Koloskov <koloskovnick@yandex.ru>
2024-10-10 21:29:41 +0300

    Initial commit of 12th Spring-1

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreDbStorage.java
@@ -12,12 +12,12 @@ import java.util.Collection;
 public class GenreDbStorage implements GenreStorage {
 
     private final JdbcTemplate jdbcTemplate;
-    private final String GENRE_SQL = "select * from genres";
+    private final static String GENRE = "select * from genres";
 
     @Override
     public Genre getGenreById(Integer genreId) {
         try {
-            return jdbcTemplate.queryForObject(GENRE_SQL.concat(" where id = ?"), new GenreMapper(), genreId);
+            return jdbcTemplate.queryForObject(GENRE.concat(" where id = ?"), new GenreMapper(), genreId);
         } catch (Exception e) {
             return null;
         }
@@ -25,6 +25,6 @@ public class GenreDbStorage implements GenreStorage {
 
     @Override
     public Collection<Genre> getAllGenres() {
-        return jdbcTemplate.query(GENRE_SQL.concat(" order by id asc"), new GenreMapper());
+        return jdbcTemplate.query(GENRE.concat(" order by id asc"), new GenreMapper());
     }
 }
