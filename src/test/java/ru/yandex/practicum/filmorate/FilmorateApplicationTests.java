a665287b4569a06aed44598242362883e2c729ac
Nikolay Koloskov <koloskovnick@yandex.ru>
2024-10-10 21:34:47 +0300

    Initial commit of 12th Spring-2

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -17,18 +17,4 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmorateApplicationTests {
-
-    private final UserStorage userStorage;
-
-    @Test
-    public void testFindUserById() {
-
-        Optional<User> userOptional = Optional.of(userStorage.getUserById(Long.valueOf(1)));
-
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
-                );
-    }
 }
